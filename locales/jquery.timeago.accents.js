1dc3bfdfc6dd51c7f59abfd10f3c746bfb5ba552
Craig Silverstein <csilvers@khanacademy.org>
2013-04-22 14:48:09 -0700

    Manually add in versions of the timeago info for our 'fake' languages.
    Summary:
    I decided this was easier than marking up a template file with
    $._("...") and letting fake_translate.py do the translation: I didn't
    know how to handle the %d's in these format strings.  I may end up
    regretting that if these files change a lot, but it will probably be
    ok.
    
    Manually add in versions of the timeago info for our 'fake' languages.
    
    I decided this was easier than marking up a template file with
    $._("...") and letting fake_translate.py do the translation: I didn't
    know how to handle the %d's in these format strings.  I may end up
    regretting that if these files change a lot, but it will probably be
    ok.
    
    Fix this file to make it actually utf-8.
    
    Add a symlink to the 'correct' locale file for the current language.
    
    This defaults to English.  When we copy this directory tree over to
    other languages, we'll magically change this symlink to point to the
    right file for that language.
    
    Test Plan:
    Fingers crossed.
    
    Fingers crossed.
    
    Reviewers: benkomalo
    
    Reviewed By: benkomalo
    
    Differential Revision: http://phabricator.khanacademy.org/D2152

---
+++ locales/jquery.timeago.accents.js
@@ -0,0 +1,19 @@
+jQuery.timeago.settings.strings = {
+  prefixAgo: null,
+  prefixFromNow: null,
+  suffixAgo: "ágò",
+  suffixFromNow: "fřöm ñȯw",
+  seconds: "lęŝŝ tĥân ấ mıŋůțè",
+  minute: "ăböųť ä mıŋůțè",
+  minutes: "%d mıŋůțès",
+  hour: "ăböųť än ḥȯüř",
+  hours: "ăböųť %d ḥȯüřs",
+  day: "ä ďåý",
+  days: "%d ďåýs",
+  month: "ăböųť ä møñtħ",
+  months: "%d møñtħs",
+  year: "ăböųť ä ýềąr",
+  years: "%d ýềąŗș",
+  wordSeparator: " ",
+  numbers: []
+};
